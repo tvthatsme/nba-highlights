1021422319dad0a76395790c5e3d28822a7cebfc
Timothy Vernon <timothy.n.vernon@gmail.com>
2019-02-03 18:10:32 +0100

    Make video play in modal

--- src/components/highlight-video.js
+++ src/components/highlight-video.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import YouTube from 'react-youtube'
 import styled from 'styled-components'
+import Modal from 'react-modal'
 import HighlightThumbnail from './highlight-thumbnail'
+import { AriaOnlyH2 } from '../theme/aria.js'
 
 const HighlightCard = styled.div`
   background-color: #111;
@@ -15,45 +17,75 @@ const Description = styled.p`
   padding: 0 24px;
 `
 
+// https://developers.google.com/youtube/player_parameters
+const youtubeOptions = {
+  playerVars: {
+    autoplay: 1,
+    controls: 1,
+    modestbranding: 1,
+  },
+}
+
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    zIndex: 1,
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#000',
+    border: 'none',
+  },
+}
+
 class HighlightVideo extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      video: false,
+      modalIsOpen: false,
     }
   }
 
-  handleClick = () => {
-    this.setState({
-      video: true,
-    })
+  openVideoModal = () => {
+    this.setState({ modalIsOpen: true })
   }
 
-  opts = {
-    height: '180',
-    width: '320',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
+  closeModal = e => {
+    e.stopPropagation()
+    this.setState({ modalIsOpen: false })
+  }
+
+  componentDidMount() {
+    Modal.setAppElement('#___gatsby')
   }
 
   render() {
     return (
-      <HighlightCard onClick={this.handleClick}>
-        {!this.state.video && (
-          <HighlightThumbnail
-            src={this.props.thumbnail}
-            alt={this.props.video.snippet.title}
-          />
-        )}
-        {this.state.video && (
-          <YouTube videoId={this.props.id} opts={this.opts} />
-        )}
+      <HighlightCard onClick={this.openVideoModal}>
+        <HighlightThumbnail
+          src={this.props.thumbnail}
+          alt={this.props.video.snippet.title}
+        />
         <Description>
           {this.props.video.snippet.title.split('|')[0].trim()}
         </Description>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Highlight Video Modal"
+          shouldCloseOnOverlayClick={true}
+        >
+          <AriaOnlyH2>{this.props.video.snippet.title}</AriaOnlyH2>
+          <YouTube videoId={this.props.id} opts={youtubeOptions} />
+        </Modal>
       </HighlightCard>
     )
   }
---
+++ src/theme/aria.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const AriaOnlyH2 = styled.h2`
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+`
