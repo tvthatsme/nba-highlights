2b49abb38b756df78c5687d4e8f69ae46fa59b0f
Timothy Vernon <timothy.n.vernon@gmail.com>
2019-02-18 22:01:23 +0100

    Fix svg style definitions

--- src/icons/play-circle.js
+++ src/icons/play-circle.js
@@ -8,10 +8,9 @@ const PlayCircle = ({ width = 24, height = 24 }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="white"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="feather feather-play-circle"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <circle cx="12" cy="12" r="10" />
     <polygon points="10 8 16 12 10 16 10 8" fill="white" />
