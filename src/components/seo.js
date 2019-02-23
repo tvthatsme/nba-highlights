3540ba35427bbf65014863af4ce08126f86bbf1a
Timothy Vernon <timothy.n.vernon@gmail.com>
2019-02-23 10:59:01 +0100

    Implement useStaticQuery in seo component [skip ci]

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,95 +1,91 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 // TODO: Figure out a way to get a dynamic image url
 const highlightThumbnail = `https://i.ytimg.com/vi/ZY5jGzRgY8k/mqdefault.jpg`
 
 function SEO({ description, lang, meta, keywords, title }) {
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(detailsQuery)
+  const metaDescription = description || siteMetadata.description
+
   return (
-    <StaticQuery
-      query={detailsQuery}
-      render={data => {
-        const metaDescription =
-          description || data.site.siteMetadata.description
-        return (
-          <Helmet
-            htmlAttributes={{
-              lang,
-            }}
-            title={title}
-            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-            meta={[
-              {
-                name: `description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:locale`,
-                content: `en_US`,
-              },
-              {
-                property: `og:title`,
-                content: data.site.siteMetadata.title,
-              },
-              {
-                property: `og:description`,
-                content: metaDescription,
-              },
-              {
-                property: `og:type`,
-                content: `website`,
-              },
-              {
-                property: `og:url`,
-                content: data.site.siteMetadata.url,
-              },
-              {
-                property: `og:site_name`,
-                content: data.site.siteMetadata.title,
-              },
-              {
-                property: `og:image`,
-                content: highlightThumbnail,
-              },
-              {
-                name: `twitter:card`,
-                content: `summary`,
-              },
-              {
-                name: `twitter:site`,
-                content: data.site.siteMetadata.url,
-              },
-              {
-                name: `twitter:creator`,
-                content: data.site.siteMetadata.author,
-              },
-              {
-                name: `twitter:title`,
-                content: data.site.siteMetadata.title,
-              },
-              {
-                name: `twitter:description`,
-                content: metaDescription,
-              },
-              {
-                name: `twitter:image`,
-                content: highlightThumbnail,
-              },
-            ]
-              .concat(
-                keywords.length > 0
-                  ? {
-                      name: `keywords`,
-                      content: keywords.join(`, `),
-                    }
-                  : []
-              )
-              .concat(meta)}
-          />
-        )
+    <Helmet
+      htmlAttributes={{
+        lang,
       }}
+      title={title}
+      titleTemplate={`%s | ${siteMetadata.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:locale`,
+          content: `en_US`,
+        },
+        {
+          property: `og:title`,
+          content: siteMetadata.title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          property: `og:url`,
+          content: siteMetadata.url,
+        },
+        {
+          property: `og:site_name`,
+          content: siteMetadata.title,
+        },
+        {
+          property: `og:image`,
+          content: highlightThumbnail,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:site`,
+          content: siteMetadata.url,
+        },
+        {
+          name: `twitter:creator`,
+          content: siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: siteMetadata.title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+        {
+          name: `twitter:image`,
+          content: highlightThumbnail,
+        },
+      ]
+        .concat(
+          keywords.length > 0
+            ? {
+                name: `keywords`,
+                content: keywords.join(`, `),
+              }
+            : []
+        )
+        .concat(meta)}
     />
   )
 }
