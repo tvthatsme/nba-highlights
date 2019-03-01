ff60668b4f6eba234c90912d8c9447f4067f94c2
Timothy Vernon <timothy.n.vernon@gmail.com>
2019-03-01 21:57:02 +0100

    Add sitemap configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
